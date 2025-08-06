a591eaeb8fac338436ddf0e85e1e13246ae3c159
Valerie <valeriealexarivera@gmail.com>
2025-08-06 14:53:07 -0400

    added @State property wrapper and modifications

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -1,24 +1,20 @@
-//
-//  ContentView.swift
-//  InteractiveUI
-//
-//  Created by Scholar on 8/6/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
                 .padding()
-            Button("Submit Name ") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            Button("Submit Name") {
+               textTitle = "Welcome, \(name)!"
+                name = ""
             }//end of button
             .font(.title2)
             .buttonStyle(.borderedProminent)
