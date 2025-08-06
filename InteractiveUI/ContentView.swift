c91f5d130e4cc19d2b9241413f6765465ecb6ada
Valerie <valeriealexarivera@gmail.com>
2025-08-06 14:31:11 -0400

    add textfield

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,7 +12,9 @@ struct ContentView: View {
         VStack {
             Text("What is your name?")
                 .font(.title)
-        }
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            
+        }// end of vstack
         .padding()
     }
 }
